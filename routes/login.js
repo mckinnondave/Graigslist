1524da7db7cb6b434468815dd920e3fa752c6af9
Dustin Grof <dustingrof@gmail.com>
2022-04-09 20:06:32 -0700

    added login routes

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,5 @@
 /*
- * All routes for Users are defined here
+ * All routes for login are defined here
  * Since this file is loaded in server.js into api/users,
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
@@ -9,18 +9,28 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then((data) => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+  // set user cookie when logged in
+  router.get("/:id", (req, res) => {
+    // using encrypted cookies
+    req.session.user_id = req.params.id;
+    // send the user back to home once logged in
+    res.redirect("/user");
   });
   return router;
 };
+
+// module.exports = (db) => {
+
+// router.get("/user", (req, res) => {
+//   res.render("user");
+// });
+
+// // logout a user
+// router.post("/logout", (req, res) => {
+//   req.session = null;
+//   res.redirect("/");
+// });
+// };
 
 // Browse /users get redirects somewhere
 // Read /users/:id show an idividuals profile
