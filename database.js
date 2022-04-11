04f3cdfc815f1726eaa04f32fc9e0fe7a9d26173
Dustin Grof <dustingrof@gmail.com>
2022-04-11 13:08:46 -0700

    Revert "midway commit on search functionality"
    This reverts commit 9258a1eeeb2a8f552db1403f38f2d6009e3a1539.

--- database.js
+++ database.js
@@ -1,20 +1,19 @@
 // PG database client/connection setup
 // not sure about this, if not working... check with a mentor
-// const { Pool } = require("pg");
-// const dbParams = require("./lib/db.js");
-// const db = new Pool(dbParams);
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
 
-const getSearchResults = (options, db)=>{
+const getSearchResults = (options)=>{
   const queryParams = [];
   let getSearchResultsQuery = `
   SELECT listings.*, users.name as user_name
     FROM listings
     JOIN users ON creator_id = users.id
-    // WHERE listings.name LIKE '%$1%'
     ORDER BY name
     LIMIT 3;
   `;
-  return db
+  return pool
     .query(getSearchResultsQuery, queryParams)
     .then((result) => {
       // console.log(result.rows);
@@ -25,7 +24,7 @@ const getSearchResults = (options, db)=>{
     });
 };
 
-module.exports = {getSearchResults};
+exports.getSearchResults = getSearchResults;
 
 
 
--- routes/search.js
+++ routes/search.js
@@ -1,41 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const dbHelpers = require();
 
-module.exports = (db, dbHelpers) => {
+module.exports = (db) => {
   // we are using GET, should we use post somehow?
   router.get("/", (req, res) => {
-    const searchParams = req.params;
-    if (!searchParams) {
-      // something in here
-    };
-    // need some if statements here to make sure not empty search field
-    //should find this in tiny app as an example.
-    dbHelpers
-      .getSearchResults(searchParams, db)
-      .then((result) => {
-        // console.log("result", result);
-        res.send(result);
-        const templateVars = { result }; //edit this to send key value pairs...
-        res.render("listings", templateVars);
-        // console.log("dustins mind is broken here");
-      })
-      .catch((e) => {
-        console.error(e);
-        res.send(e);
-      });
+    const searchParams = req.params; //probably not right
+    db.getSearchResults(searchParams).then(() => {
+      console.log("dustins mind is broken here");
+    });
     console.log("this is the search route speaking");
-    // res.redirect("/user");
+    req.query();
+    res.redirect("/user");
   });
   return router;
 };
-
-// router.get("/properties", (req, res) => {
-//   database
-//     .getAllProperties(req.query, 20)
-//     .then((properties) => res.send({ properties }))
-//     .catch((e) => {
-//       console.error(e);
-//       res.send(e);
-//     });
-// });
