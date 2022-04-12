b064a75069d1bb8be6181711e2ebe30e12029a06
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-04-12 23:21:01 +0000

    add route for user:ids

---
+++ routes/user.js
@@ -0,0 +1,47 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const { promiseImpl } = require("ejs");
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db, dbHelpers) => {
+  router.get("/", (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.send({ message: "not logged in" });
+      return;
+    }
+    db.query(`SELECT * FROM users;`)
+      .then((data) => {
+        const users = data.rows;
+        res.render("user");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    const userId = req.params.id;
+    Promise.all([
+      dbHelpers.getUserInfo(userId, db),
+      dbHelpers.getUserItems(userId, db),
+    ])
+      .then((results) => {
+        const templateVars = { results: results[0], userItems: results[1] };
+        console.log(templateVars);
+        res.render("user", templateVars);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  return router;
+};
