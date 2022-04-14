a8f14da5d98aa3db1790f4b4429fb2f9a6af09d0
Emily Bozek <emily.bozek@gmail.com>
2022-04-13 20:38:44 -0700

    post to add favourite and delete

--- routes/favourite.js
+++ routes/favourite.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const router = express.Router();
 
-// id,user_id,listing_id
-module.exports = () => {
+module.exports = (db) => {
   router.post("/", (req, res) => {
-    console.log(req.body)
+    console.log("FAVOURITES REQ BODY", req.body)
     const userId = req.session.userId;
-    const listingId = req.session.listing
-    sql = `INSERT
-    INTO favourites
+    const listingId = req.body.listingId;
+    console.log("INSERT LISTING ID", listingId)
+    const sql = `INSERT
+    INTO favourites (user_id, listing_id)
+    VALUES ($1, $2)
+    RETURNING *
     `
+    //AVOID DUPLICATES
+    db.query(sql, [userId, listingId])
+    .then((result) => {
+      res.send(result.rows)
+      console.log("INSERT RESULT ROWS", result.rows)
+    })
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
   });
+
+  router.post("/delete", (req, res) => {
+    const { dataId } = req.body;
+    console.log("DELETE LISTING ID", dataId)
+    const sql = `DELETE
+    FROM listings
+    WHERE id = ${dataId}
+    `
+    db.query(sql)
+    .then(() => {
+      res.send("Ok")
+    })
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+  });
+
   return router;
 };
+
+
